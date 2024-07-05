09d776b1ffd0819947310130abf954555621349e
HeberthDev <heberthdeveloper@gmail.com>
2024-07-05 08:19:20 -0300

    Consumindo a API Json

--- script-crud.js
+++ script-crud.js
@@ -17,5 +17,5 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     };
 
     tarefas.push(tarefa);
-    localStorage.setItem('tarefas', tarefas);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 });
